5a9050bb398790c7113c014811caa90abfba14c0
Josh Rose <joshrose@hey.com>
2023-03-13 08:05:41 -0400

    WIP: containers

--- examples/nate-cool-examples/main.tf
+++ examples/nate-cool-examples/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     cockroachgke = {
-      source = "terraform.local/local/cockroachgke"
+      source  = "terraform.local/local/cockroachgke"
       version = "1.0.0"
     }
   }
@@ -11,5 +11,16 @@ provider "cockroachgke" {
   host     = "localhost"
   username = "nate"
   password = "nate"
-  certpath = "/users/nate/certs/ca.crt"
+  certpath = "../../certs/certs/ca.crt"
+}
+
+resource "cockroachgke_database" "nate_db" {
+  name = "nate_db"
+}
+
+resource "cockroachgke_user" "nate_user" {
+  username   = "nate2"
+  password   = "natepw"
+  database   = cockroachgke_database.nate_db.name
+  privileges = ["select", "update", "insert"]
 }
