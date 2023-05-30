46ea3e9b0db42512ce7978d796467301c5c98754
Nate Schlie <schlie.nt@pg.com>
2023-05-30 15:13:21 -0400

    deleting changefeeds

--- internal/provider/changefeed_resource.go
+++ internal/provider/changefeed_resource.go
@@ -27,6 +27,7 @@ type ChangefeedResourceModel struct {
 	BucketName types.String `tfsdk:"bucket"`
 	Token      types.String `tfsdk:"token"`
 	Database   types.String `tfsdk:"database"`
+	JobID      types.String `tfsdk:"job_id"`
 }
 
 // Metadata appends the resource name to the provider name
@@ -54,6 +55,10 @@ func (r *ChangefeedResource) Schema(ctx context.Context, req resource.SchemaRequ
 			"database": schema.StringAttribute{
 				MarkdownDescription: "Database for the tables receiving a changefeed",
 				Required:            true,
+			},
+			"job_id": schema.StringAttribute{
+				MarkdownDescription: "ID returned for the changefeed",
+				Computed:            true,
 			},
 		},
 	}
@@ -86,6 +91,7 @@ func (r *ChangefeedResource) Create(ctx context.Context, req resource.CreateRequ
 		)
 		return
 	}
+	defer client.Close()
 
 	database := strings.Replace(data.Database.String(), "\"", "", -1)
 	table := strings.Replace(data.TableName.String(), "\"", "", -1)
@@ -93,16 +99,16 @@ func (r *ChangefeedResource) Create(ctx context.Context, req resource.CreateRequ
 	token := strings.Replace(data.Token.String(), "\"", "", -1)
 	query := fmt.Sprintf("SET DATABASE=%s; CREATE CHANGEFEED FOR TABLE %s INTO 'gs://%s?AUTH=specified&CREDENTIALS=%s';", database, table, bucket, token)
 
-	// id := client.QueryRow()
-	_, err = client.Exec(query)
+	var id string
+	err = client.QueryRow(query).Scan(&id)
 	if err != nil {
 		resp.Diagnostics.AddError("Create changefeed error", fmt.Sprintf("Unable to create changefeed, got error: %s", err))
 		return
 	}
+	data.JobID = types.StringValue(id)
 
-	// Write logs using the tflog package
 	// Documentation: https://terraform.io/plugin/log
-	tflog.Trace(ctx, "created a resource")
+	tflog.Trace(ctx, "created a changefeed")
 
 	// Save data into Terraform state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
@@ -162,13 +168,27 @@ func (r *ChangefeedResource) Delete(ctx context.Context, req resource.DeleteRequ
 		return
 	}
 
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
-	// httpResp, err := r.client.Do(httpReq)
-	// if err != nil {
-	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to delete example, got error: %s", err))
-	//     return
-	// }
+	client, err := r.db.Connect()
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Failed to connect to cockroach",
+			err.Error(),
+		)
+		return
+	}
+	defer client.Close()
+
+	db := strings.Replace(data.Database.String(), "\"", "", -1)
+	id := strings.Replace(data.JobID.String(), "\"", "", -1)
+
+	query := fmt.Sprintf("SET DATABASE=%s; CANCEL JOB %s;", db, id)
+	_, err = client.Exec(query)
+	if err != nil {
+		resp.Diagnostics.AddError("Delete changefeed error", fmt.Sprintf("Unable to delete changefeed, got error: %s", err))
+		return
+	}
+
+	tflog.Trace(ctx, "deleted a changefeed")
 }
 
 func (r *ChangefeedResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
